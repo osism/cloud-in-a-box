ae2e059fa2c6a9e32c9733659fa8e8f545839149
Christian Berendt <berendt@osism.tech>
2023-01-24 11:01:59 +0100

    Add missing wait_for_container_healthy to bootstrap.sh
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,20 @@
 
 export INTERACTIVE=false
 
+wait_for_container_healthy() {
+    local max_attempts="$1"
+    local name="$2"
+    local attempt_num=1
+
+    until [[ "$(/usr/bin/docker inspect -f '{{.State.Health.Status}}' $name)" == "healthy" ]]; do
+        if (( attempt_num++ == max_attempts )); then
+            return 1
+        else
+            sleep 5
+        fi
+    done
+}
+
 apt-get update
 apt-get install -y python3-virtualenv sshpass
 
