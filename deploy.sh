788d4c3ac88356d52150732f9a12d38ef24d0ee5
Christian Berendt <berendt@osism.tech>
2023-09-18 17:42:42 +0200

    Deploy k8s (#128)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- deploy.sh
+++ deploy.sh
@@ -83,6 +83,21 @@ osism apply --environment openstack bootstrap-ceph-rgw
 osism apply netdata
 osism apply openstackclient
 
+osism apply k3s
+
+# NOTE: The following lines will be moved to an osism.services.clusterapi role
+export KUBECONFIG=$HOME/.kube/config
+kubectl label node manager openstack-control-plane=enabled
+sudo curl -Lo /usr/local/bin/clusterctl https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.4.4/clusterctl-linux-amd64
+sudo chmod +x /usr/local/bin/clusterctl
+export EXP_CLUSTER_RESOURCE_SET=true
+export CLUSTER_TOPOLOGY=true
+clusterctl init \
+  --core cluster-api:v1.4.4 \
+  --bootstrap kubeadm:v1.4.4 \
+  --control-plane kubeadm:v1.4.4 \
+  --infrastructure openstack:v0.7.1
+
 osism apply wireguard
 
 mv /home/dragon/wg0-dragon.conf /home/dragon/wireguard-client.conf
