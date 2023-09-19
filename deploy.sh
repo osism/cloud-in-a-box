9de8c3b1c042a1b0c46e33b0f2e0af534d89d22d
Christian Berendt <berendt@osism.tech>
2023-09-19 19:35:15 +0200

    Use CAPO_VERSION & CAPI_VERSION for CAPI & CAPI versions (#139)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- deploy.sh
+++ deploy.sh
@@ -99,15 +99,18 @@ osism manage images --cloud admin --filter "Ubuntu 22.04 Minimal"
 
 osism apply k3s
 
+CAPI_VERSION="v1.5.1"
+CAPO_VERSION="v0.8.0"
+
 # NOTE: The following lines will be moved to an osism.services.clusterapi role
 export KUBECONFIG=/home/dragon/.kube/config
 kubectl label node manager openstack-control-plane=enabled
-sudo curl -Lo /usr/local/bin/clusterctl https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.5.1/clusterctl-linux-amd64
+sudo curl -Lo /usr/local/bin/clusterctl https://github.com/kubernetes-sigs/cluster-api/releases/download/${CAPI_VERSION}/clusterctl-linux-amd64
 sudo chmod +x /usr/local/bin/clusterctl
 export EXP_CLUSTER_RESOURCE_SET=true
 export CLUSTER_TOPOLOGY=true
 clusterctl init \
-  --core cluster-api:v1.5.1 \
-  --bootstrap kubeadm:v1.5.1 \
-  --control-plane kubeadm:v1.5.1 \
-  --infrastructure openstack:v0.8.0
+  --core cluster-api:${CAPI_VERSION} \
+  --bootstrap kubeadm:${CAPI_VERSION} \
+  --control-plane kubeadm:${CAPI_VERSION} \
+  --infrastructure openstack:${CAPO_VERSION}
