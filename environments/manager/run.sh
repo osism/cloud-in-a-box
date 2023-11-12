aa2b7cb8f2c0428097bd62d0f5bd728f6b255899
Christian Berendt <berendt@osism.tech>
2023-11-12 17:27:50 +0100

    Sync with cfg-generics
    MANAGER_VERSION=6.0.1 gilt overlay
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- environments/manager/run.sh
+++ environments/manager/run.sh
@@ -6,6 +6,10 @@ ANSIBLE_COLLECTION_COMMONS_VERSION=${ANSIBLE_COLLECTION_COMMONS_VERSION:-main}
 ANSIBLE_COLLECTION_SERVICES_VERSION=${ANSIBLE_COLLECTION_SERVICES_VERSION:-main}
 ANSIBLE_PLAYBOOKS_MANAGER_VERSION=${ANSIBLE_PLAYBOOKS_MANAGER_VERSION:-main}
 
+ANSIBLE_COLLECTION_COMMONS_SOURCE=${ANSIBLE_COLLECTION_COMMONS_SOURCE:-git+https://github.com/osism/ansible-collection-commons}
+ANSIBLE_COLLECTION_SERVICES_SOURCE=${ANSIBLE_COLLECTION_SERVICES_SOURCE:-git+https://github.com/osism/ansible-collection-services}
+ANSIBLE_PLAYBOOKS_MANAGER_SOURCE=${ANSIBLE_PLAYBOOKS_MANAGER_SOURCE:-git+https://github.com/osism/ansible-playbooks-manager}
+
 INSTALL_ANSIBLE=${INSTALL_ANSIBLE:-true}
 INSTALL_ANSIBLE_ROLES=${INSTALL_ANSIBLE_ROLES:-true}
 VENV_PATH=${VENV_PATH:-.venv}
@@ -43,10 +47,18 @@ command -v ansible-galaxy >/dev/null 2>&1 || { echo >&2 "ansible-galaxy not inst
 ANSIBLE_USER=${ANSIBLE_USER:-dragon}
 CLEANUP=${CLEANUP:-false}
 
+cleanup () {
+    if [[ $CLEANUP == "true" ]]; then
+        rm id_rsa.operator
+        rm -rf "$VENV_PATH"
+    fi
+}
+trap cleanup ERR EXIT
+
 if [[ $INSTALL_ANSIBLE_ROLES == "true" ]]; then
-    ansible-galaxy collection install -f "git+https://github.com/osism/ansible-collection-commons,${ANSIBLE_COLLECTION_COMMONS_VERSION}"
-    ansible-galaxy collection install -f "git+https://github.com/osism/ansible-collection-services,${ANSIBLE_COLLECTION_SERVICES_VERSION}"
-    ansible-galaxy collection install -f "git+https://github.com/osism/ansible-playbooks-manager,${ANSIBLE_PLAYBOOKS_MANAGER_VERSION}"
+    ansible-galaxy collection install -f "${ANSIBLE_COLLECTION_COMMONS_SOURCE},${ANSIBLE_COLLECTION_COMMONS_VERSION}"
+    ansible-galaxy collection install -f "${ANSIBLE_COLLECTION_SERVICES_SOURCE},${ANSIBLE_COLLECTION_SERVICES_VERSION}"
+    ansible-galaxy collection install -f "${ANSIBLE_PLAYBOOKS_MANAGER_SOURCE},${ANSIBLE_PLAYBOOKS_MANAGER_VERSION}"
 fi
 
 if [[ ! -e id_rsa.operator ]]; then
@@ -54,7 +66,7 @@ if [[ ! -e id_rsa.operator ]]; then
         -i localhost, \
         -e @../secrets.yml \
         -e "keypair_dest=$(pwd)/id_rsa.operator" \
-        osism.manager.keypair "$@"
+        osism.manager.keypair "$@" || exit $?
 fi
 
 if [[ $playbook == "k8s" || $playbook == "netbox" || $playbook == "traefik" ]]; then
@@ -71,7 +83,7 @@ if [[ $playbook == "k8s" || $playbook == "netbox" || $playbook == "traefik" ]]; 
         -e @configuration.yml \
         -e @secrets.yml \
         -u "$ANSIBLE_USER" \
-        osism.manager."$playbook" "$@"
+        osism.manager."$playbook" "$@" || exit $?
 elif [[ $playbook == "operator" ]]; then
     if [[ $ANSIBLE_ASK_PASS == "True" ]]; then
         ansible-playbook \
@@ -83,7 +95,7 @@ elif [[ $playbook == "operator" ]]; then
             -e @configuration.yml \
             -e @secrets.yml \
             -u "$ANSIBLE_USER" \
-            osism.manager."$playbook" "$@"
+            osism.manager."$playbook" "$@" || exit $?
     else
         ansible-playbook \
             --private-key id_rsa.operator \
@@ -95,7 +107,7 @@ elif [[ $playbook == "operator" ]]; then
             -e @configuration.yml \
             -e @secrets.yml \
             -u "$ANSIBLE_USER" \
-            osism.manager."$playbook" "$@"
+            osism.manager."$playbook" "$@" || exit $?
     fi
 else
     ansible-playbook \
@@ -108,10 +120,5 @@ else
         -e @configuration.yml \
         -e @secrets.yml \
         -u "$ANSIBLE_USER" \
-        osism.manager."$playbook" "$@"
-fi
-
-if [[ $CLEANUP == "true" ]]; then
-    rm id_rsa.operator
-    rm -rf "$VENV_PATH"
+        osism.manager."$playbook" "$@" || exit $?
 fi
