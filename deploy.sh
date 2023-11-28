3365b02068492a669b4212e1ef7a5c5a203f1356
Christian Berendt <berendt@osism.tech>
2023-11-28 16:36:19 +0100

    Use osism manage flavors
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- deploy.sh
+++ deploy.sh
@@ -55,7 +55,6 @@ osism apply copy-ceph-keys
 osism apply cephclient
 osism apply ceph-bootstrap-dashboard
 
-
 osism apply keystone
 osism apply horizon
 osism apply skyline
@@ -66,6 +65,7 @@ osism apply nova
 osism apply cinder
 osism apply designate
 osism apply octavia
+
 osism apply kolla-ceph-rgw
 
 if [[ $CLOUD_IN_A_BOX_TYPE == "sandbox" ]]; then
@@ -95,6 +95,9 @@ osism apply --environment openstack bootstrap-$CLOUD_IN_A_BOX_TYPE
 # Upload machine images
 osism manage images --cloud admin --filter Cirros
 osism manage images --cloud admin --filter "Ubuntu 22.04 Minimal"
+
+# Create machine types
+osism manage flavors
 
 # Create test project (without a server and attached volume)
 osism apply --environment openstack test --skip-tags test-server,test-volume
