97b0e1f48fd0f1e7bcee57686378b4c7fcaf98f0
Christian Berendt <berendt@osism.tech>
2024-03-04 08:55:08 +0100

    Use git pull to update the configuration repository (#233)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- upgrade-manager.sh
+++ upgrade-manager.sh
@@ -9,5 +9,11 @@ set -e
 export INTERACTIVE=false
 source /etc/cloud-in-a-box.env
 
-osism apply configuration
+# The normal way of updating the configuration repository (osism apply configuration)
+# is not used as we have made manual changes in the configuration repository
+# on the Cloud in a Box. For example, for the primary network card.
+pushd /opt/configuration
+git pull
+popd
+
 osism update manager
