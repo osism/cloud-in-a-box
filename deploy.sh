29c8323adc69603eab132e8c526cc5df79662d10
Christian Berendt <berendt@osism.tech>
2024-03-25 14:48:20 +0100

    Use ceph-pools play to create ceph pools (#258)
    Part of osism/issues#909
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- deploy.sh
+++ deploy.sh
@@ -48,6 +48,7 @@ osism reconciler sync
 
 # Deploy Ceph services
 osism apply ceph -e enable_ceph_mds=true -e enable_ceph_rgw=true
+osism apply ceph-pools
 osism apply copy-ceph-keys
 osism apply cephclient
 osism apply ceph-bootstrap-dashboard
